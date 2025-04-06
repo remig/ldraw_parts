97affba55e99c467950ff56c330df3e2e54bf45f
Remi <remigagne@gmail.com>
2025-04-06 08:20:45 -0700

    Update gen script to make sense

--- optimize_parts.py
+++ optimize_parts.py
@@ -1,49 +1,71 @@
 #!/usr/bin/env python3
 
 import os, sys, re, shutil, glob
+from pathlib import Path
+
+j = os.path.join
 
 if len(sys.argv) < 2:
 	sys.exit((
-		"\nUsage: optimize_parts output_path\n"
-		"Optimize each LDraw part in this script's directory then put the result in 'output_path'\n"
+		"\nUsage: optimize_parts input_ldraw_lib_path\n"
+		"Optimize each LDraw part in the specified `input_ldraw_lib_path` folder then put the result in this repo's local `parts` folder\n"
 	))
 
 os.chdir('./')
-outpath = sys.argv[1]
+inLdrawPath = sys.argv[1]
+outpath = './parts_new'
 
-if not os.path.isdir(outpath):
-	os.mkdir(outpath)
+if not os.path.isdir(inLdrawPath):
+	print(f'error: specified LDraw path {inLdrawPath} does not exist')
+elif not os.path.isdir(j(inLdrawPath, 'parts')):
+	print(f'error: specified LDraw path {inLdrawPath} does not contain necessary"parts" folder')
+elif not os.path.isdir(j(inLdrawPath, 'p')):
+	print(f'error: specified LDraw path {inLdrawPath} does not contain necessary "p" folder')
 
-for fn in glob.glob(os.path.join(outpath, '*')):
-	os.remove(fn)
+for fn in glob.glob(j(outpath, '*')):
+	if not os.path.isdir(fn):
+		os.remove(fn)
 
-shutil.copyfile('LDConfig.ldr', os.path.join(outpath, 'LDConfig.ldr'))
-shutil.copyfile('LDConfig_lic.ldr', os.path.join(outpath, 'LDConfig_lic.ldr'))
-os.mkdir(os.path.join(outpath, 'parts'))
-os.mkdir(os.path.join(outpath, 'parts', 's'))
-os.mkdir(os.path.join(outpath, 'parts', '8'))
-os.mkdir(os.path.join(outpath, 'parts', '48'))
+shutil.copyfile(j(inLdrawPath, 'LDConfig.ldr'), "LDConfig_original.ldr")
+
+Path(outpath).mkdir(exist_ok=True)
+Path(j(outpath, 's')).mkdir(exist_ok=True)
+Path(j(outpath, '8')).mkdir(exist_ok=True)
+Path(j(outpath, '48')).mkdir(exist_ok=True)
 
 trimWhitespace = re.compile('\n\ns+')
+trimComments = re.compile('//.*$')
 
-for idx, fn in enumerate(glob.glob('parts/**', recursive=True)):
+def fixOneFile(idx, fullFn, fn):
 	try:
-		if os.path.isdir(fn):
-			continue
+		if os.path.isdir(fullFn):
+			return
 		lines = []
 		if idx % 100 == 0:
 			print('.', end='')
-		with open(fn, encoding='utf-8') as f:
-			for i, line in enumerate(f):
+		with open(fullFn, encoding='utf-8') as f:
+			for line in f:
+				line = re.sub(trimComments, '', line)
 				line = re.sub(trimWhitespace, ' ', line).strip()
 				if line.startswith('0 !HISTORY') or line.startswith('0 !LDRAW') or len(line) < 3:
 					continue
 				lines.append(line + '\n')
 
-		with open(os.path.join(outpath, fn), 'w', encoding='utf-8') as f:
+		with open(j(outpath, fn), 'w', encoding='utf-8') as f:
 			f.writelines(lines)
 	except:
-		print('\n**Failed to fix file:', fn)
+		print('\n**Failed to fix file:', fullFn)
 		print('error:' , sys.exc_info()[0])
 		print('value:' , sys.exc_info()[1])
 		print('stack:' , sys.exc_info()[2])
+
+for idx, fn in enumerate(glob.glob(j(inLdrawPath, 'p/**'), recursive=True)):
+	if fn.endswith('png'):
+		continue
+	fixOneFile(idx, fn, re.sub('^.*/p/', '', fn))
+
+for idx, fn in enumerate(glob.glob(j(inLdrawPath, 'parts/**'), recursive=True)):
+	if fn.endswith('png'):
+		continue
+	fixOneFile(idx, fn, re.sub('^.*/parts/', '', fn))
+
